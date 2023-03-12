7aa056432ef0858661f827944af28d547de959ea
DanielaParra <dparralp396@gmail.com>
2023-03-11 20:22:52 -0500

    Removed unneeded initState property from data

--- src/data/checkboxes.ts
+++ src/data/checkboxes.ts
@@ -1,134 +1,108 @@
 type CheckboxType = {
     label: string,
-    id: string,
-    initialState: boolean
+    id: string
 }
 
 export const checkboxes: CheckboxType[] = [
     {
         label: "Alcohol-Free",
         id: "alcoholFree",
-        initialState: false
     },
     {
         label: "Celery-Free",
         id: "celeryFree",
-        initialState: false
     },
     {
         label: "Crustacean-Free",
         id: "crustaceanFree",
-        initialState: false
     },
     {
         label: "Dairy-Free",
         id: "dairyFree",
-        initialState: false
     },
     {
         label: "Egg-Free",
         id: "eggFree",
-        initialState: false
     },
     {
         label: "Fish-Free",
         id: "fishFree",
-        initialState: false
     },
     {
         label: "FODMAP-Free",
         id: "fodmapFree",
-        initialState: false
     },
     {
         label: "Gluten-Free",
         id: "glutenFree",
-        initialState: false
     },
     {
         label: "Kidney-Friendly",
         id: "kidneyFriendly",
-        initialState: false
     },
     {
         label: "Kosher",
         id: "kosher",
-        initialState: false
     },
     {
         label: "Lupine-free",
         id: "lupineFree",
-        initialState: false
     },
     {
         label: "Mollusk-Free",
         id: "molluskFree",
-        initialState: false
     },
     {
         label: "Mustard-Free",
         id: "mustardFree",
-        initialState: false
     },
     {
         label: "Peanut-Free",
         id: "peanutFree",
-        initialState: false
     },
     {
         label: "Pescatarian",
         id: "pescatarian",
-        initialState: false
     },
     {
         label: "Pork-Free",
         id: "porkFree",
-        initialState: false
     },
     {
         label: "Read Meat-Free",
         id: "readMeatFree",
-        initialState: false
     },
     {
         label: "Sesame-Free",
         id: "sesameFree",
-        initialState: false
     },
     {
         label: "Shellfish-Free",
         id: "shellfishFree",
-        initialState: false
     },
     {
         label: "Soy-Free",
         id: "soyFree",
-        initialState: false
     },
     {
         label: "Sulfite-Free",
         id: "sulfiteFree",
-        initialState: false
     },
     {
         label: "Tree Nut-Free",
         id: "treeNutFree",
-        initialState: false
     },
     {
         label: "Vegan",
         id: "vegan",
-        initialState: false
     },
     {
         label: "Vegetarian",
         id: "vegetarian",
-        initialState: false
     },
     {
         label: "Wheat-Free",
         id: "wheatFree",
-        initialState: false
     }
 
 ]
