1f058135e9acb337f4638ef835289fcf1b488e43
DanielaParra <dparralp396@gmail.com>
2023-03-13 17:27:16 -0400

    Updating readme and folder names from firestore to firebase. Removed unneceesary imports in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
-import { useState } from 'react';
-import db from './firestore/firebase';
 import Header from './components/Header/Header';
 import Main from './components/Main/Main';
 
 
 function App() {
   
-
   return (
     <div className="App">
       <Header />
