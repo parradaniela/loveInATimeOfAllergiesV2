705e22f3fe1989029d8ef72fc7dedb070b6aaa4a
DanielaParra <dparralp396@gmail.com>
2023-03-16 19:39:37 -0400

    Updated onChange and map logic based on updated Context data. Deleted unnecessary code

--- src/components/Main/dropdown/Dropdown.tsx
+++ src/components/Main/dropdown/Dropdown.tsx
@@ -1,42 +1,31 @@
 import { ChangeEvent, useContext } from "react"
 import { DataContext } from "../../../context/DataContext";
 
-type PartyDataType = {
-        guestName: string,
-        party: string, 
-        restrictions: string[]
-}
-
 const Dropdown = () => {
     const { firebaseData, userChoice, setUserChoice } = useContext(DataContext)
-    // const allParties = firebaseData.map(data => Object.values(data))
-    // console.log(allParties);
-    // const data = [...firebaseData]
-    // console.log(data);
-    
-    
-    // const partyNames = Object.keys(allParties)
-    
+
     return (
-    <div>
-        {/* <label htmlFor="partyChoice">Choose Your Party</label>
-        <select 
-            name="partyChoice" 
-            id="partyChoice"
-            onChange={(e: ChangeEvent<HTMLSelectElement>) => setUserChoice(e.target.value)}
-            value={userChoice}
-        >
-            <option value="" disabled>Choose a party</option>
-            {
-                partyNames.map((party) => {
-                    return (
-                        <option value={party} key={party}>{party}</option>
-                    )
-                })
-            }
-        </select> */}
-    </div>
-  )
+        <div>
+            <label htmlFor="partyChoice">Choose Your Party</label>
+            <select
+                name="partyChoice"
+                id="partyChoice"
+                //Using optional chaining here to check that setUserChoice is not undefined then executes it, once again due to the Partial typing in the Context
+                onChange={(e: ChangeEvent<HTMLSelectElement>) => setUserChoice?.(e.target.value)}
+                value={userChoice}
+            >
+                <option value="" disabled>Choose a party</option>
+                {
+                    // Using the non-null assertion operator because creating the Context with a Partial Interface means that all of the values can be undefined. So I have to assert that the firebaseData will not be undefined
+                    (Object.keys(firebaseData!)).map((party) => {
+                        return (
+                            <option value={party} key={party}>{party}</option>
+                        )
+                    })
+                }
+            </select>
+        </div>
+    )
 }
 
 export default Dropdown
