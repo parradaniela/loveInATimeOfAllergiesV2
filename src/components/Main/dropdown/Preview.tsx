38c83b5275b848578035a9abf9613b38bbdc7caa
DanielaParra <dparralp396@gmail.com>
2023-03-16 19:43:54 -0400

    Added useEffect that updates the preview display. Added some JSX to confirm things update correctly
    JSX still needs to be completed correctly

--- src/components/Main/dropdown/Preview.tsx
+++ src/components/Main/dropdown/Preview.tsx
@@ -1,6 +1,41 @@
+import { useContext, useEffect } from "react"
+import { DataContext } from "../../../context/DataContext"
+
 const Preview = () => {
+
+  const { firebaseData, userChoice, partyPreviewObj, setPartyPreviewObj } = useContext(DataContext)
+
+  useEffect(() => {
+    for (let key in firebaseData) {
+      if (key === userChoice) {
+        setPartyPreviewObj?.(firebaseData[key as keyof {}]);
+      }
+    }
+  }, [userChoice, firebaseData])
+
+  const parties = Object.values(partyPreviewObj!)
+
   return (
-    <div>Preview</div>
+    <div className="guestList">
+      <div className="summaryContainer cssanimation">
+        <h2>Step 3: Review Your Party</h2>
+        <ul className="partySummary">
+          <li className="guestName">
+            <h3>Name</h3>
+          </li>
+          {
+            parties.map((party, index) => {
+              return (
+                <div key={index}>
+                  <p>{party.guestName}</p>
+                  <p>{party.restrictions}</p>
+                </div>
+              )
+            })
+          }
+        </ul>
+      </div>
+    </div>
   )
 }
 
