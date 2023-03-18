902e0501731330c2ebfca3ed0a834d5687d8d71c
DanielaParra <dparralp396@gmail.com>
2023-03-18 19:33:43 -0400

    Reconfigured apiCall to export relevant function(s). Added onSubmit() to SelectForm and async callApi() to SelectForm
    Both component have some commented out code for now, will revisit to clean up once flow of functions is finalized.

---
+++ src/components/Main/recipeDisplay/apiCall.ts
@@ -0,0 +1,38 @@
+import { PartyPreviewDataType } from "../../../context/DataContext";
+  
+  const edamamAppID = "app_id=d42468c4";
+  const edamamApiKey = "app_key=2a81448b298b7c6715d72883fbd595cd"
+  const mealType = "mealType=dinner"
+  const random = "random=true"
+  export const apiEndpoint = `https://api.edamam.com/api/recipes/v2?type=public&${edamamAppID}&${edamamApiKey}&${mealType}&${random}`
+
+
+  const checkForRestrictions = (partyPreviewData: PartyPreviewDataType) => {
+    const filteredData = partyPreviewData.filter(data => data.restrictions)
+    return filteredData
+  }
+
+  export const constructApiParams = (partyPreviewData: PartyPreviewDataType) => {
+    const filteredData = checkForRestrictions(partyPreviewData)
+    if (filteredData.length > 0) {
+      const mergedRestrictions = filteredData.map(guestObj => {
+        // console.log(mergedRestrictions);
+          return guestObj.restrictions
+      });
+      const concatenated = Array.prototype.concat.apply([], mergedRestrictions)
+      // console.log(concatenated);
+      const unique = [...new Set(concatenated)]
+      // console.log(unique);
+      const finalUrlParams = unique.join("&health=")
+      // console.log(finalUrlParams);
+      return `&health=${finalUrlParams}`
+    }
+    return ''
+  }
+
+  // export const callApi = async <T>(restrictionParams: string) => {
+  //   const apiData = await fetch(`${apiEndpoint}${restrictionParams}`)
+  //   const recipes = await apiData.json()
+  //   return recipes.hits
+  // }
+
