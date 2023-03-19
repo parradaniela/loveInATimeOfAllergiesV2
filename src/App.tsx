9076d19f31ce53dde0dafefb1297c470198f49a0
DanielaParra <dparralp396@gmail.com>
2023-03-19 16:34:13 -0400

    Added Footer folder to match other component folders

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ import './css/recipes.css';
 import './css/mediaQueries.css';
 import Header from './components/Header/Header';
 import Main from './components/Main/Main';
-import Footer from './components/Footer';
+import Footer from './components/Footer/Footer';
 
 
 function App() {
