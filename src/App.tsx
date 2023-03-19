d50cd8230e537ad0495c3b59e6f4a3a86df3da10
DanielaParra <dparralp396@gmail.com>
2023-03-18 21:56:46 -0400

    Separated CSS into components, replaced some colours with variables

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,16 @@
-import Footer from './components/Footer';
+import './css/setup.css';
+import './css/variables.css';
+import './css/typography.css';
+import './css/animations.css';
+import './css/general.css';
+import './css/headerFooter.css';
+import './css/checkboxes.css';
+import './css/preview.css';
+import './css/recipes.css';
+import './css/mediaQueries.css';
 import Header from './components/Header/Header';
 import Main from './components/Main/Main';
+import Footer from './components/Footer';
 
 
 function App() {
